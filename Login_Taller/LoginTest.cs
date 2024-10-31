3bd7ee864004d73b46cec1b3b489362763f3e3bc
Carlos Ruiz <cruizfreak@hotmail.com>
2024-10-31 17:38:06 -0600

    Add project files.

---
+++ Login_Taller/LoginTest.cs
@@ -0,0 +1,26 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace Login_Taller
+{
+    public class Tests
+    {
+
+        [Test]
+        public void IngresoCorrecto()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
+            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            driver.FindElement(By.CssSelector("#login button")).Click();
+
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
