5c4ace84179708994510bc56e9d77183f79353a7
Carlos Ruiz <cruizfreak@hotmail.com>
2024-10-31 17:45:44 -0600

    Se agrego un nuevo test

--- Login_Taller/LoginTest.cs
+++ Login_Taller/LoginTest.cs
@@ -16,6 +16,23 @@ namespace Login_Taller
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
 
             driver.FindElement(By.Id("username")).SendKeys("tomsmith");
+            Thread.Sleep(500);
+            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            Thread.Sleep(500);
+            driver.FindElement(By.CssSelector("#login button")).Click();
+            Thread.Sleep(500);
+
+            driver.Close();
+            driver.Quit();
+        }
+        [Test]
+        public void IngresoIncorrecto()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+            driver.FindElement(By.Id("username")).SendKeys("tomsmith1");
             driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
             driver.FindElement(By.CssSelector("#login button")).Click();
 
